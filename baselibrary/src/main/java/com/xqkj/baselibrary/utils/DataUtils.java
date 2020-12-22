04e86760b64b1aa18bd563103690509bcc6c2173
adench@126.com <liyunlong930211>
2020-12-22 16:00:42 +0800

    数据工具类

---
+++ baselibrary/src/main/java/com/xqkj/baselibrary/utils/DataUtils.java
@@ -0,0 +1,39 @@
+package com.xqkj.baselibrary.utils;
+
+import android.text.TextUtils;
+
+import java.util.List;
+import java.util.regex.Pattern;
+
+public class DataUtils {
+
+    //时间不足两位，0补齐
+    public static String formatData(int str){
+        return String.format("%2d", str).replace(" ", "0");
+    }
+
+    public static boolean isNumberic(String str){
+        Pattern pattern = Pattern.compile("[0-9]*");
+        return pattern.matcher(str).matches();
+    }
+
+    /**
+     * 数组转文字串
+     * @param listK
+     * @return
+     */
+    public static String lis2String(List<?> listK, String tager){
+        if(listK == null)return "";
+        tager = TextUtils.isEmpty(tager)?",":tager;
+        String str = listK.toString();
+        str = str.replace("[","").replace("]","").replace(", ",tager);
+        return str;
+    }
+
+    /**
+     * 更换中间间隔符号
+     */
+    public static String replaceTag(String str, String oldTag, String newTag){
+        return str.replace(oldTag,newTag);
+    }
+}
