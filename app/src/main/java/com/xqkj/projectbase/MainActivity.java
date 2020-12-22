653c1259a0e82372503b65c99dbd1cef6a5b5969
adench@126.com <liyunlong930211>
2020-12-22 16:27:31 +0800

    修改引入依赖

--- app/src/main/java/com/xqkj/projectbase/MainActivity.java
+++ app/src/main/java/com/xqkj/projectbase/MainActivity.java
@@ -4,11 +4,16 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
 
+import com.xqkj.baselibrary.utils.DataUtils;
+
+
 public class MainActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        DataUtils.formatData(1001);
     }
 }
