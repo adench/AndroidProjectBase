5689e893d4e29aaa70c21eecac27340fc9f4d7a7
adench@126.com <liyunlong930211>
2021-04-23 16:05:46 +0800

    修改引入包位置

--- app/src/main/java/com/xqkj/projectbase/base/AppApplication.java
+++ app/src/main/java/com/xqkj/projectbase/base/AppApplication.java
@@ -1,6 +1,5 @@
 package com.xqkj.projectbase.base;
 
-import com.lzy.okgo.model.HttpHeaders;
 import com.xqkj.baselibrary.base.BaseApplication;
 import com.xqkj.baselibrary.net.HttpSetting;
 
@@ -9,9 +8,6 @@ public class AppApplication extends BaseApplication {
     public void onCreate() {
         super.onCreate();
 
-        //配置请求头
-        HttpHeaders headers = HttpSetting.headers;
-        HttpSetting.headers = headers;
         //配置请求成功状态码
         HttpSetting.SUCCESS_CODE = "";
 
