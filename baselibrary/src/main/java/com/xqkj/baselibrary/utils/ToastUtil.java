363e071595ed84fef2adc54dbae29e342d6b09c5
adench@126.com <liyunlong930211>
2021-05-25 11:00:27 +0800

    toast修改

--- baselibrary/src/main/java/com/xqkj/baselibrary/utils/ToastUtil.java
+++ baselibrary/src/main/java/com/xqkj/baselibrary/utils/ToastUtil.java
@@ -7,10 +7,21 @@ import android.widget.Toast;
 import com.xqkj.baselibrary.base.BaseApplication;
 
 public class ToastUtil {
+
     public static void showToast(String showStr){
+        showCenterToast(showStr);
+    }
+
+    public static void showCenterToast(String showStr){
         if(TextUtils.isEmpty(showStr))return;
         Toast toast = Toast.makeText(BaseApplication.mContext, showStr, Toast.LENGTH_SHORT);
         toast.setGravity(Gravity.CENTER,0,0);
+        toast.show();
+    }
+
+    public static void showBottomToast(String showStr){
+        if(TextUtils.isEmpty(showStr))return;
+        Toast toast = Toast.makeText(BaseApplication.mContext, showStr, Toast.LENGTH_SHORT);
         toast.show();
     }
 
